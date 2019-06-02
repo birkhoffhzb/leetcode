7b2785b0579d55cfde751b24da0aae8b0a4d50e6
birkhoffhzb <birkhoffhzb@163.com>
2019-06-02 20:31:00 +0800

    1.两数之和

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,11 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
 
 
 public class TwoSum {
     public static void main(String[] args) {
-
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum2(nums, target);
+        System.out.println(result[0] + " " + result[1]);
     }
 
+    /*
+    通过两重循环进行遍历，判断num[i] + nums[j] == target,然后输出结果即可。
+     */
     public static int[] twoSum(int[] nums, int target) {
-        return new int[]{1, 2};
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    /*
+    上述方法虽然可以解决问题，但是由于需要进行两重循环，因此复杂度为O(n^2),因此寻求更高效的方式。
+    利用HashMap的结构，将出现的值存在map中的key，这样就可以O(1)的时间获得，将复杂度降低为O(n)。
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
     }
 }
