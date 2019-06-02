cbfa9685cb6ccbaba132af31165bb76eef9d486c
birkhoffhzb <birkhoffhzb@163.com>
2019-06-02 20:06:39 +0800

    test commit

---
+++ src/TwoSum.java
@@ -0,0 +1,11 @@
+
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        return new int[]{1, 2};
+    }
+}
